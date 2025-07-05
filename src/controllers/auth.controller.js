fa115fbc7bb687e19c4e5e74d56ceb96649648a2
SachinBalagam <balagamsachin337@gmail.com>
2025-07-05 20:56:14 +0530

    http removed in cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -129,7 +129,7 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-        httpOnly: true,
+        httpOnly: false,
         secure: isProd,
         sameSite: isProd ? "None" : "Lax",
       })
